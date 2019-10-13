81ebc45df1670e2d48d4efc7bf1e2dbe1d2e64d9
Altaf-dev <53747685+Altaf-dev@users.noreply.github.com>
2019-10-13 22:39:43 +0600

    пробую javascript

--- script/script.js
+++ script/script.js
@@ -1,12 +1,17 @@
-document.querySelector(".info1").onclick = function(){
-    this.classList.toggle("info-open");
+document.querySelector('.info1').onclick = function(){
+    this.classList.toggle('info-open')
+
 };
-document.querySelector(".info2").onclick = function(){
-    this.classList.toggle("info-open");
+document.querySelector('.info2').onclick = function(){
+    this.classList.toggle('info-open')
 };
-document.querySelector(".info3").onclick = function(){
-    this.classList.toggle("info-open");
+document.querySelector('.info3').onclick = function(){
+    this.classList.toggle('info-open')
 };
-document.querySelector(".info4").onclick = function(){
-    this.classList.toggle("info-open");
+document.querySelector('.info3').onclick = function(){
+    this.classList.toggle('info-open')
+};
+document.querySelectorAll("info-open").onclick = function () {
+    this.classList.toggle("about-rotate:after");
+
 };
