5703344aed11fc20b0c51acf603737c375186ed0
Altaf-dev <53747685+Altaf-dev@users.noreply.github.com>
2019-10-13 19:04:56 +0600

    пробую javascript

--- script/script.js
+++ script/script.js
@@ -1,12 +1,12 @@
 document.querySelector(".info1").onclick = function(){
     this.classList.toggle("info-open");
-}
+};
 document.querySelector(".info2").onclick = function(){
     this.classList.toggle("info-open");
-}
+};
 document.querySelector(".info3").onclick = function(){
     this.classList.toggle("info-open");
-}
+};
 document.querySelector(".info4").onclick = function(){
     this.classList.toggle("info-open");
-}
+};
